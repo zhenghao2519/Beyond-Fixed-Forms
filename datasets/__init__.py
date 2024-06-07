e861386eb4ed4d7f6f61470274e5a5b28805b413
Jie Hu <hujiejason@foxmail.com>
2024-06-07 20:22:40 +0200

    Merged branch "branch1"

---
+++ datasets/__init__.py
@@ -0,0 +1,17 @@
+from datasets.replica_loader import ReplicaReader
+from datasets.scannet_loader import ScanNetReader
+from datasets.s3dis_loader import S3DISReader
+
+
+__all__ = ["ReplicaReader", "ScanNetReader", "build_dataset"]
+
+
+def build_dataset(root_path, cfg):
+    if cfg.data.dataset_name == "scannet200" or cfg.data.dataset_name == "scannetpp":
+        return ScanNetReader(root_path, cfg)
+    elif cfg.data.dataset_name == "replica":
+        return ReplicaReader(root_path, cfg)
+    elif cfg.data.dataset_name == "s3dis":
+        return S3DISReader(root_path, cfg)
+    else:
+        raise ValueError(f"Unknown dataset: {cfg.data.dataset_name}")
