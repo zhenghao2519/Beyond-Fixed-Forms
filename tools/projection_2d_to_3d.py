de61810349f116a3f9fb814795743f5e77a3dfb4
zhenghao <zhenghao@gmail.com>
2024-06-08 20:15:16 +0200

    add detected ratio threshold

--- tools/projection_2d_to_3d.py
+++ tools/projection_2d_to_3d.py
@@ -13,6 +13,7 @@ import argparse
 from configs import config as cfg
 from munch import Munch
 
+from tqdm import tqdm
 import sys
 
 sys.path.append("/medar_smart/temp/Beyond-Fixed-Forms/tools")
@@ -200,7 +201,7 @@ if __name__ == "__main__":
     masks_2d_path = os.path.join(mask_2d_dir, f"{scene_id}.pth")
     gronded_sam_results = torch.load(masks_2d_path)
 
-    all_points_masked = torch.zeros(scene_pcd.shape[1]).to(
+    masked_counts = torch.zeros(scene_pcd.shape[1]).to(
         device=device
     )  # scene_pcd has shape (4, N)
     for i in range(len(gronded_sam_results)):  # range(35,40):
@@ -208,19 +209,12 @@ if __name__ == "__main__":
         frame_id = gronded_sam_results[i]["frame_id"][:-4]
         print("-------------------------frame", frame_id, "-------------------------")
         segmented_frame_masks = gronded_sam_results[i]["segmented_frame_masks"]
-        # segmented_frame_masks = rle_decode(gronded_sam_results[i]["segmented_frame_masks_rle"])    # TODO: HERE IS THE INFERENCE RESULTS OF ALL FRAMES (A DICTIONARY)
 
         pred_masks = segmented_frame_masks.squeeze(dim=1).numpy()  # (M, H, W)
-        # print(pred_masks.shape, pred_masks.sum(axis=(1,2)), pred_masks.max())
-        # map 3d pointclouds to 2d
         cam_pose = np.loadtxt(os.path.join(cam_pose_dir, f"{frame_id}.txt"))
 
         scene_pts = copy.deepcopy(scene_pcd)
         scene_pts = (np.linalg.inv(cam_pose) @ scene_pts).T[:, :3]  # (N, 3)
-        # print(scene_pts.shape, np.max(scene_pts[:,2]), np.min(scene_pts[:,2]))
-        # pcd = copy.deepcopy(pcd).transform(np.linalg.inv(cam_pose)) # world to camera
-        # scene_pts = np.asarray(pcd.points)
-        # projected_pts = compute_projected_pts(scene_pts, cam_intr)
         projected_pts = compute_projected_pts_tensor(scene_pts, cam_intr)
 
         photo_width = int(cfg.width_2d)
@@ -233,34 +227,16 @@ if __name__ == "__main__":
             / depth_scale
         )
         depth_im = cv2.resize(depth_im, (photo_width, photo_height))  # Width x Height
-        # print("depth image shape:" , depth_im.shape, "mask shape",  pred_masks.shape)
-        # plt.imshow(depth_im, cmap='gray')
-        # plt.colorbar(label='Depth (meters)')
-        # plt.title('Depth Image Visualization')
-        # plt.show()
-        # # depth_im = depth_im/depth_scale
-        # visibility_mask = compute_visibility_mask(
-        #     scene_pts, projected_pts, depth_im, depth_thresh=0.08
-        # )
         visibility_mask = compute_visibility_mask_tensor(
             scene_pts, projected_pts, depth_im, depth_thresh=0.08
         )
-        # assert np.all(visibility_mask == visibility_mask_tensor)
-        # visibility_count[visibility_mask] += 1
-        # print(depth_im.max())
-        # print("visibility check result", visibility_mask.sum())
 
-        # masked_pts = compute_visible_masked_pts(
-        #     scene_pts, projected_pts, visibility_mask, pred_masks
-        # )  # (M, N)
         masked_pts = compute_visible_masked_pts_tensor(
             scene_pts, projected_pts, visibility_mask, pred_masks
         )
-        # assert np.all(masked_pts == masked_pts_tensor)
+
 
         masked_pts = torch.from_numpy(masked_pts).to(device)
-        # if ground_indices is not None:
-        #     masked_pts[:, ground_indices] = 0
         mask_area = torch.sum(masked_pts, dim=1).detach().cpu().numpy()  # (M,)
         print(
             "number of 3d mask points:",
@@ -270,27 +246,77 @@ if __name__ == "__main__":
         )
 
         for mask in masked_pts:
-            # print("single_mask shape", mask.shape, "all mask shape", all_points_masked.shape)
-            all_points_masked[mask] += 1
+            # print("single_mask shape", mask.shape, "all mask shape", masked_counts.shape)
+            masked_counts[mask] += 1
 
-    occurance_counts = all_points_masked.unique()
-    print("occurance count", all_points_masked.unique())
-    occurance_thres = cfg.occurance_threshold
-    occurance_thres_value = occurance_counts[
-        round(occurance_thres * occurance_counts.shape[0])
-    ]
-    print("occurance thres value", occurance_thres_value)
 
-    # remove all the points under median occurance
-    all_points_masked[all_points_masked < occurance_thres_value] = 0
+    ## Start filtering
+    if cfg.if_occurance_threshold:
+        occurance_counts = masked_counts.unique()
+        print("occurance count", masked_counts.unique())
+        occurance_thres = cfg.occurance_threshold
+        occurance_thres_value = occurance_counts[
+            round(occurance_thres * occurance_counts.shape[0])
+        ]
+        print("occurance thres value", occurance_thres_value)
 
-    all_points_masked = all_points_masked > 0
-    print("final masked points", all_points_masked.sum())
+        # remove all the points under median occurance
+        masked_counts[masked_counts < occurance_thres_value] = 0
+
+    elif cfg.if_detected_ratio_threshold:
+        scene_id = cfg.scene_id
+        root_dir = cfg.root_dir
+        image_dir = os.path.join(root_dir, scene_id, "color")
+
+        image_files = [f for f in os.listdir(image_dir) if f.endswith('.jpg')]
+        image_files.sort(key=lambda x: int(x.split('.')[0]))         # sort numerically, 1.jpg, 2.jpg, 3.jpg ...
+        downsampled_image_files = image_files[::10]                  # get one image every 10 frames
+        downsampled_images_paths = [os.path.join(image_dir, f) for f in downsampled_image_files]
+
+        viewed_counts = torch.zeros(scene_pcd.shape[1]).to(device=device)
+        for i, image_path in enumerate(tqdm(downsampled_images_paths, desc="Calculating viewed counts for every point", leave=False)):
+            frame_id = image_path.split('/')[-1][:-4]
+            cam_pose = np.loadtxt(os.path.join(cam_pose_dir, f"{frame_id}.txt"))
+
+            scene_pts = copy.deepcopy(scene_pcd)
+            scene_pts = (np.linalg.inv(cam_pose) @ scene_pts).T[:, :3]  # (N, 3)
+            projected_pts = compute_projected_pts_tensor(scene_pts, cam_intr)
+
+            photo_width = int(cfg.width_2d)
+            photo_height = int(cfg.height_2d)
+            depth_im_dir = cfg.depth_im_dir
+            # frame_id_num = frame_id.split('.')[0]
+            depth_im_path = os.path.join(cfg.depth_im_dir, f"{frame_id}.png")
+            depth_im = (
+                cv2.imread(depth_im_path, cv2.IMREAD_UNCHANGED).astype(np.float32)
+                / depth_scale
+            )
+            depth_im = cv2.resize(depth_im, (photo_width, photo_height))  # Width x Height
+            visibility_mask = compute_visibility_mask_tensor(
+                scene_pts, projected_pts, depth_im, depth_thresh=0.08
+            )
+            viewed_counts += torch.tensor(visibility_mask).to(device=device)
+
+        # only calculate non-zero viewed counts
+        print("viewed_counts", viewed_counts.unique())
+        detected_ratio = masked_counts / (viewed_counts+1) # avoid /0
+        print("detected_ratio", detected_ratio.unique())
+        detected_ratio_thres = cfg.detected_ratio_threshold
+        detected_ratio_thres_value = detected_ratio.unique()[round(detected_ratio_thres * detected_ratio.unique().shape[0])]
+        print("detected_ratio_thres_value", detected_ratio_thres_value)
+        masked_counts[detected_ratio < detected_ratio_thres_value] = 0
+
+
+
+
+
+    masked_counts = masked_counts > 0
+    print("final masked points", masked_counts.sum())
 
     os.makedirs(os.path.join(cfg.mask_3d_dir, cfg.base_prompt), exist_ok=True)
     torch.save(
         {
-            "ins": all_points_masked.unsqueeze(0),
+            "ins": masked_counts.unsqueeze(0),
             "conf": torch.tensor([0.36]),
             "final_class": torch.tensor([30]),
         },
