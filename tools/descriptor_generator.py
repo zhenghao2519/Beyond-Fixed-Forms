fcf766a6d55fdb98d19c38fb40cd239c09bd507a
Jie Hu <hujiejason@foxmail.com>
2024-06-12 20:39:43 +0200

    seperate helper functions out to make the original code less cluttered

---
+++ tools/descriptor_generator.py
@@ -0,0 +1,29 @@
+################################################################################################################
+# This file contains the functions that generate descriptors for the prompts.
+
+from typing import List
+
+
+def toy_descriptors(base_prompt):          
+    '''
+    toy function to mock the extending of prompts
+    '''
+    descriptors = ["that is on the floor","that has furry ears","that $2@zj9it8/%j",",which 8aoiU(&*YT)"]
+    return descriptors
+
+def generate_descriptors_waffle(base_prompt) ->List[str]:
+    # TBD
+    descriptors = []
+    return descriptors
+
+def generate_descriptors_gpt(base_prompt) ->List[str]:
+    # TBD
+    descriptors = []
+    return descriptors
+
+def generate_descriptors_waffle_and_gpt(base_prompt) ->List[str]:
+    # TBD
+    descriptors = []
+    return descriptors
+
+# And so on, TODO: fill in the functions
---
+++ tools/utils.py
@@ -0,0 +1,25 @@
+################################################################################################################
+# Utility functions, can move helper functions here to make original code less cluttered
+
+# useful if predict_extended took a list of base_prompt
+def print_dict(dict_descriptors):
+    '''
+    print the dict of descriptors, where
+    dict_descriptors = {class:[extended captions for the class]}
+    '''
+    for k, v in dict_descriptors.items():
+        for descriptor in v:
+            print(f" {k} - {descriptor}")
+
+def combine_base_and_descriptors(dict_descriptors) ->List[List[str]]:
+    '''
+    args: dict_descriptors = {class:[extended captions for the class]}
+    returns: combined = [[class, descriptors], [class, descriptors], ...]
+    '''
+    combined = []
+    for k, v in dict_descriptors.items():
+        extended = []
+        for descriptor in v:
+            extended.append(f"{k}, {descriptor}")
+        combined.append(extended)
+    return combined
